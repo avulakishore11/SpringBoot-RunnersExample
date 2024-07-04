d816c12b6192a5c56ed85119ddeec796ccf5b82d
avulakishore11 <134989441+avulakishore11@users.noreply.github.com>
2024-07-03 21:33:37 -0400

    Update RunnersExampleApplication.java

--- src/main/java/com/tw/RunnersExampleApplication.java
+++ src/main/java/com/tw/RunnersExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RunnersExampleApplicationTests {
+public class RunnersExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RunnersExampleApplicationTests.class, args);
+		SpringApplication.run(RunnersExampleApplication.class, args);
 	}
 
 }
