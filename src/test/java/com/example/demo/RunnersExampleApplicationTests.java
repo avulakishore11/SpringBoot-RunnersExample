2aeb60e1e17e88067ef316e26366f3405bc5797d
avulakishore11 <134989441+avulakishore11@users.noreply.github.com>
2024-07-03 20:19:03 -0400

    Update RunnersExampleApplicationTests.java

--- src/test/java/com/example/demo/RunnersExampleApplicationTests.java
+++ src/test/java/com/example/demo/RunnersExampleApplicationTests.java
@@ -5,9 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest(classes = RunnersExampleApplication.class)
+@SpringBootTest(classes = DemoApplication.class)
 @RunWith(SpringRunner.class)
-public class RunnersExampleApplicationTests {
+public class DemoApplication {
 
 	@Test
 	public void contextLoads() {
